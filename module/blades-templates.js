ee72616cab8f966de78ed0faa23904018c335d6c
Olivier Grech <olivier.grech@tutanota.com>
2020-12-23 17:41:44 +0100

    Start Cult abilities

--- module/blades-templates.js
+++ module/blades-templates.js
@@ -20,6 +20,7 @@ export const preloadHandlebarsTemplates = async function() {
     "systems/blades-in-the-dark/templates/actor-parts/attributes.html",
     "systems/blades-in-the-dark/templates/actor-parts/items.html",
     "systems/blades-in-the-dark/templates/actor-parts/stress.html",
+    "systems/blades-in-the-dark/templates/crew-parts/crew-abilities-cult.html",
     "systems/blades-in-the-dark/templates/crew-parts/crew-coin.html",
     "systems/blades-in-the-dark/templates/crew-parts/crew-generic.html",
     "systems/blades-in-the-dark/templates/crew-parts/crew-heat.html",
